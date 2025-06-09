91cc38bed86c23e2e7d0015b6b62354f3af50cfe
sollhaile <sollhaile192@gmail.com>
2025-06-09 12:55:59 +0300

    creating Django-Middleware-0x03/middleware requests.log

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,20 +1,12 @@
 # chats/middleware.py
-import logging
 from datetime import datetime
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # Configure logger
-        logging.basicConfig(
-            filename='requests.log',
-            level=logging.INFO,
-            format='%(message)s'
-        )
-        self.logger = logging.getLogger(__name__)
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else 'Anonymous'
-        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
-        self.logger.info(log_message)
+        with open('requests.log', 'a') as log_file:
+            log_file.write(f"{datetime.now()} - User: {user} - Path: {request.path}\n")
         return self.get_response(request)
