ee37a94e3e2d51887c50fa992d8278c887abd381
sollhaile <sollhaile192@gmail.com>
2025-06-15 10:48:37 +0300

    update Django-Middleware-0x03/register to setting.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,21 +1,12 @@
-import time
-from django.utils.deprecation import MiddlewareMixin
+import datetime
 
-class RequestLogMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        request.start_time = time.time()
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-    def process_response(self, request, response):
-        total_time = time.time() - getattr(request, 'start_time', time.time())
-        log_line = (
-            f"METHOD: {request.method} | "
-            f"PATH: {request.get_full_path()} | "
-            f"STATUS: {response.status_code} | "
-            f"TIME: {total_time:.4f} seconds\n"
-        )
-
-        log_path = "C:/Users/Y.S/Documents/alx-backend-python/Django-Middleware-0x03/requests.log"
-        with open(log_path, "a") as f:
-            f.write(log_line)
-
+    def __call__(self, request):
+        log_line = f"{datetime.datetime.now()} - {request.method} {request.path}\n"
+        with open("C:/Users/Y.S/Documents/alx-backend-python/Django-Middleware-0x03/requests.log", "a") as log_file:
+            log_file.write(log_line)
+        response = self.get_response(request)
         return response
