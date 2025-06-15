c21c5d1f358e78750a01f0190e5f102924ec09cd
sollhaile <sollhaile192@gmail.com>
2025-06-15 23:54:17 +0300

    Django-signals-orm-0x04

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -30,6 +30,7 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)  # track edits
-
+    edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='edited_messages')
     def __str__(self):
         return f'{self.sender.username}: {self.content[:20]}'
