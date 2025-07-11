27113ba4f9ed839668ee106ee84d0c0db81c28b7
sollhaile <sollhaile192@gmail.com>
2025-07-11 14:46:49 +0300

    Messaging app Updated

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # messaging_app/settings.py
@@ -135,3 +136,4 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+STATIC_ROOT = BASE_DIR / "staticfiles"
